35bd021827b8770c242dac4af67611bfd1870f42
Jonas Carvalho <jonasmcarvalho@outlook.com>
2022-04-25 07:53:28 -0300

    Applying Styling

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -17,13 +17,12 @@ export function TopRelatedMovies() {
     const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=de1fe62e293e7bb93774da73df634cb3&language=pt-BR&page=1`)
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=de1fe62e293e7bb93774da73df634cb3&language=pt-BR&page=2`)
             .then(response => response.json())
             .then(data => data.results)
             .then(dataFin => setRepositories(dataFin))            
         }, [])
 
-        console.log(process.env.KEY)
 
     return (
         <Container>
--- src/components/MoviesList/styles.ts
+++ src/components/MoviesList/styles.ts
@@ -2,32 +2,40 @@ import styled from "styled-components";
 
 
 export const Container = styled.section`
-    margin: 40px;
+    margin: 2rem;
 
     h1 {
-        margin-bottom: 16px;
+        margin-bottom: 4rem;
+        justify-content: center;
+        display: flex;
     }
 
     ul {
-        list-style: none;
+        list-style: none;        
 
         li {
+            background: var(--shape);
+            padding: 1rem 2rem;
+            line-height: 1.5rem;
+            text-align: justify;
+            border-radius: 1.25rem;
+            
             & + li {
-                margin-top: 20px;
+                margin-top: 1rem;                
+            }
+
+            strong {
+                font-size: 1.3rem;
+                justify-content: center;
+                display: flex;
+                padding: 0 0 1rem 0;
             }
 
             p {
-                font-size: 14px;
-                color: #444;
-                margin-top: 8px;
-            }
-
-            a {
-                display: inline-block;
-                margin-top: 16px;
-                text-decoration: none;
-                color: #8257e6;
-            }
+                font-size: 1rem;
+                color: #444;                
+                margin-top: 0.7rem;
+            }            
         }
     }    
 `;
