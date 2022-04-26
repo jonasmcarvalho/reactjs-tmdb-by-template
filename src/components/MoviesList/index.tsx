bd1f71198b99e5cae380ff5ce2d68cc5e5bbb9b2
Jonas Carvalho <jonasmcarvalho@outlook.com>
2022-04-26 08:02:40 -0300

    Consuming API with Axios

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import { TopRelatedMoviesItem } from "./TopRelatedMoviesItem";
 
 import { Container } from "./styles";
-
-
+import { api } from "../../services/api";
 
 interface Repository {    
     title: string;
@@ -17,12 +16,9 @@ export function TopRelatedMovies() {
     const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=de1fe62e293e7bb93774da73df634cb3&language=pt-BR&page=2`)
-            .then(response => response.json())
-            .then(data => data.results)
-            .then(dataFin => setRepositories(dataFin))            
-        }, [])
-
+        api.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=de1fe62e293e7bb93774da73df634cb3&language=pt-BR&page=2`)
+            .then(response => setRepositories(response.data.results))           
+        }, []) 
 
     return (
         <Container>
